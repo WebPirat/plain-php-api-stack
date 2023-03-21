e0701bda98430f951d5c6530748463255eecee41
Dieter Koch <103990262+WebPirat@users.noreply.github.com>
2023-03-21 23:23:24 +0100

    Update JWT.php

--- classes/JWT.php
+++ classes/JWT.php
@@ -1,29 +1,48 @@
 <?php
 
-class JWT {
+class JWT
+{
     private $secret;
 
-    public function __construct($secret) {
+    public function __construct($secret)
+    {
         $this->secret = $secret;
     }
 
-    public function encode($payload) {
-        $header = base64_encode(json_encode(array('alg' => 'HS256', 'typ' => 'JWT')));
-        $payload = base64_encode(json_encode($payload));
-        $signature = hash_hmac('sha256', "$header.$payload", $this->secret, true);
-        $signature = base64_encode($signature);
-        return "$header.$payload.$signature";
+    public function encode($payload)
+    {
+        $header = json_encode([
+            'typ' => 'JWT',
+            'alg' => 'HS256'
+        ]);
+
+        $payload = json_encode($payload);
+
+        $base64UrlHeader = str_replace(['+', '/', '='], ['-', '_', ''], base64_encode($header));
+        $base64UrlPayload = str_replace(['+', '/', '='], ['-', '_', ''], base64_encode($payload));
+
+        $signature = hash_hmac('sha256', $base64UrlHeader . "." . $base64UrlPayload, $this->secret, true);
+        $base64UrlSignature = str_replace(['+', '/', '='], ['-', '_', ''], base64_encode($signature));
+
+        return $base64UrlHeader . "." . $base64UrlPayload . "." . $base64UrlSignature;
     }
 
-    public function decode($jwt) {
-        $parts = explode('.', $jwt);
-        $header = json_decode(base64_decode($parts[0]), true);
-        $payload = json_decode(base64_decode($parts[1]), true);
-        $signature = base64_decode($parts[2]);
-        $check = hash_hmac('sha256', "$parts[0].$parts[1]", $this->secret, true);
-        if (strcmp($signature, $check) !== 0) {
-            return false;
+    public function decode($jwt)
+    {
+        list($base64UrlHeader, $base64UrlPayload, $base64UrlSignature) = explode('.', $jwt);
+
+        $header = base64_decode(str_replace(['-', '_'], ['+', '/'], $base64UrlHeader));
+        $payload = base64_decode(str_replace(['-', '_'], ['+', '/'], $base64UrlPayload));
+        $signature = base64_decode(str_replace(['-', '_'], ['+', '/'], $base64UrlSignature));
+
+        $valid = hash_hmac('sha256', $base64UrlHeader . "." . $base64UrlPayload, $this->secret, true) === $signature;
+
+        if (!$valid) {
+            return null;
         }
+
+        $payload = json_decode($payload, true);
+
         return $payload;
     }
 }
